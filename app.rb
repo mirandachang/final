2a02067c43d5cf8e7a1442bf6356e839aa2215d2
mirandachang <miranda.chang@kellogg.northwestern.edu>
2020-06-08 02:21:31 +0000

    working on seed data for restaurants table

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# restaurants_table = DB.from(:restaurants)
+restaurants_table = DB.from(:restaurants)
 
 # Home Page
 get "/" do
@@ -38,7 +38,7 @@ post "/users/create" do
         (:name => params["name"],
         :email => params["email"],
         :password => BCrypt::Password.create(params["passwrord"]))
-    view '/'
+    view '/create_user'
 end
 
 get "/log-in" do
